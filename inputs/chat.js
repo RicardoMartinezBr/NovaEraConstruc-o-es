a4a1598724da450370a234fc59ebcec4d53f1fbb
RicardoMartinez <ricardoemartine@hotmail.com>
2021-03-15 22:57:05 +0100

    implemented whatsapp properly in individual js file and changes banner and text layout

---
+++ inputs/chat.js
@@ -0,0 +1,11 @@
+const chat = function () {
+    var options = {
+      whatsapp: "+55(48)99013797", // WhatsApp number
+      call_to_action: "Peça seu Orçamento Gratuito!", // Call to action
+      position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  }();
